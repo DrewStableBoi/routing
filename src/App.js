daa117de2d40c0f3ed870cd41eb294e871f5786c
taylorwoodman <taylor.woodman711@gmail.com>
2019-07-06 11:18:33 -0600

    routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+
+
+function LookAtMe(){
+  return <div style={{width: '200px', height: '200px', background: 'yellow'}}>Look at me!</div>
+}
+
+function Page1(){
+  return (
+  <div>
+    Hello from page 1
+    <Route path='/page1/hey' component={LookAtMe}/>
+
+  </div>
+  )
+}
+
+function Page2(props){
+  console.log(props)
+  return <div>Hello from page 2</div>
+}
+
+function Page3(){
+  return <div>Hello from page 3</div>
+}
+
+function MyRouter() {
+  return (
+    <Router>
+      <div style={{width: '100pw', height: '100px', background: 'blue'}}>
+        <Link 
+        to='/page1/hey'>
+          <button style={{background: 'grey', borderRadius: '33px', width: '100px', height: '20px', margin: '5px', color: 'white', outline: 'none'}}>Page 1
+          </button>
+          </Link>
+        <Link 
+        to='/page2/2345678' >
+          <button style={{background: 'grey', borderRadius: '33px', width: '100px', height: '20px', margin: '5px', color: 'white', outline: 'none'}}>Page 2
+          </button>
+          </Link>
+        <Link 
+        to='/page3'>
+          <button style={{background: 'grey', borderRadius: '33px', width: '100px', height: '20px', margin: '5px', color: 'white', outline: 'none'}}>Page 3
+          </button>
+          </Link>
+      </div>
+    <Switch>
+      <Route path='/page1' component={Page1}/>
+      <Route path='/page2/:id' component={Page2}/>
+      <Route path='/page3' component={Page3}/>
+    </Switch>
+    </Router>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyRouter />
     </div>
   );
 }
